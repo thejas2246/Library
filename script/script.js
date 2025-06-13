726a2938b9f891172d6ab8efd35277940c3222c6
Thejas A <thejas2246@gmail.com>
2025-06-13 11:29:12 +0530

    Add createBook,displayBook and removeBook functions

--- script/script.js
+++ script/script.js
@@ -1,11 +1,28 @@
 const form = document.querySelector(".form-div");
-const addButton = document.querySelector(".button");
+const mainForm = document.querySelector("form");
+const addBookButton = document.querySelector(".button");
 const cancelButton = document.querySelector(".cancel-button");
-
-addButton.addEventListener("click", toggleClass);
-cancelButton.addEventListener("click", toggleClass);
+const addButton = document.querySelector(".add-button");
+const titleInput = document.querySelector("#title");
+const authorInput = document.querySelector("#author");
+const pageInput = document.querySelector("#pages");
+let titleValue;
+let authorValue;
+let pageValue;
 
 const myLibrary = [];
+
+addBookButton.addEventListener("click", toggleClass);
+cancelButton.addEventListener("click", toggleClass);
+addButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  toggleClass();
+  titleValue = titleInput.value;
+  authorValue = authorInput.value;
+  pageValue = pageInput.value;
+  createBook(titleValue, authorValue, pageValue);
+  mainForm.reset();
+});
 
 function Book(title, author, page, uId) {
   if (!new.target) {
@@ -17,6 +34,25 @@ function Book(title, author, page, uId) {
   this.uId = uId;
 }
 
+function createBook(title, author, page) {
+  const uId = crypto.randomUUID();
+  const book = new Book(title, author, page, uId);
+  myLibrary.push(book);
+  displayBooks();
+}
+
+function displayBooks() {
+  for (let book of myLibrary) {
+    console.log(book);
+  }
+}
+function removeBook(uId) {
+  for (let i = 0; i < myLibrary.length; i++) {
+    if (myLibrary[i].uId == uId) {
+      myLibrary.splice(i, 1);
+    }
+  }
+}
 function toggleClass() {
   form.classList.toggle("show");
 }
