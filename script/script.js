0391023dbf1b11370ac410f585035db567885d17
Thejas A <thejas2246@gmail.com>
2025-06-13 13:24:23 +0530

    Display books to the screen

--- script/script.js
+++ script/script.js
@@ -6,6 +6,7 @@ const addButton = document.querySelector(".add-button");
 const titleInput = document.querySelector("#title");
 const authorInput = document.querySelector("#author");
 const pageInput = document.querySelector("#pages");
+const divContainer = document.querySelector(".book-container");
 let titleValue;
 let authorValue;
 let pageValue;
@@ -13,13 +14,17 @@ let pageValue;
 const myLibrary = [];
 
 addBookButton.addEventListener("click", toggleClass);
-cancelButton.addEventListener("click", toggleClass);
+cancelButton.addEventListener("click", function () {
+  mainForm.reset();
+  toggleClass();
+});
 addButton.addEventListener("click", function (e) {
   e.preventDefault();
   toggleClass();
   titleValue = titleInput.value;
   authorValue = authorInput.value;
   pageValue = pageInput.value;
+  console.log(pageValue);
   createBook(titleValue, authorValue, pageValue);
   mainForm.reset();
 });
@@ -38,12 +43,24 @@ function createBook(title, author, page) {
   const uId = crypto.randomUUID();
   const book = new Book(title, author, page, uId);
   myLibrary.push(book);
-  displayBooks();
+  displayBooks(book);
 }
 
 function displayBooks() {
+  divContainer.textContent = "";
   for (let book of myLibrary) {
-    console.log(book);
+    const div = document.createElement("div");
+    const h1 = document.createElement("h1");
+    const p1 = document.createElement("p");
+    const p2 = document.createElement("p");
+    div.setAttribute("class", "book-children");
+    h1.textContent = book.title;
+    p1.textContent = book.author;
+    p2.textContent = book.page;
+    div.appendChild(h1);
+    div.appendChild(p1);
+    div.appendChild(p2);
+    divContainer.appendChild(div);
   }
 }
 function removeBook(uId) {
