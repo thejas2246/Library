dc4467f8038b0cfb8f26ca954b550d1976d072f3
Thejas A <thejas2246@gmail.com>
2025-06-13 10:50:18 +0530

    Add myLibray array and Book constructor
    myLibray array to store all the book that are created
    Book constructor to create Book objects
    with title,author,pages and uId

--- script/script.js
+++ script/script.js
@@ -2,10 +2,21 @@ const form = document.querySelector(".form-div");
 const addButton = document.querySelector(".button");
 const cancelButton = document.querySelector(".cancel-button");
 
+addButton.addEventListener("click", toggleClass);
+cancelButton.addEventListener("click", toggleClass);
+
+const myLibrary = [];
+
+function Book(title, author, page, uId) {
+  if (!new.target) {
+    throw new Error("Use new keyword to call the function!");
+  }
+  this.title = title;
+  this.author = author;
+  this.page = page;
+  this.uId = uId;
+}
+
 function toggleClass() {
   form.classList.toggle("show");
 }
-
-addButton.addEventListener("click", toggleClass);
-
-cancelButton.addEventListener("click", toggleClass);
